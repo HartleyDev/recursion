1065625a71d950e3a2bcc5ebd3bd74b07ca0b164
TheDevilsKode <mrwebbin@gmail.com>
2014-05-05 13:55:56 -0700

    Finish strigifyJSON needs major refactor but works

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,6 +2,50 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
-var stringifyJSON = function(obj) {
-  // your code goes here
+
+function stringifyJSON(obj){ 
+  var type = typeof obj;
+  if(type == 'number') {
+  	num = obj;
+  	return num.toString();
+  }else if (type == 'string'){
+  	return '"' + obj + '"';
+  }else if (type == 'boolean'){
+  	return obj.toString();
+  }else if (obj === undefined){
+  	return '';
+  }else if (type == 'object'){
+  	if(!obj){
+  		return '' + obj + '';
+  	}
+  	if(Array.isArray(obj)){
+  		var stringified = '[';
+  		for(var i = 0; i < obj.length; i++){
+  			stringified += stringifyJSON(obj[i]) + ',';
+  		}
+  		stringified = stringified.replace(/,\s*$/, "");
+  		stringified += ']';
+  		return stringified;
+  	}else {
+  		var stringified = '{';
+  		for(var key in obj){
+  			if (typeof obj[key] != "function" && typeof obj[key] != "undefined"){
+	  			stringified += stringifyJSON(key);
+	  			stringified += ':' + stringifyJSON(obj[key]) + ',';
+	  		}
+  		}
+  		stringified = stringified.replace(/,\s*$/, "");
+  		stringified += '}';
+  		return stringified;
+  	}
+  }
 };
+
+
+
+
+
+
+// input: an object
+// output: object as a string
+// contstraints: 
