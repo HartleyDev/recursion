01cb8e33ebeea00e948bc69c04ff1747543e8538
TheDevilsKode <mrwebbin@gmail.com>
2014-05-06 16:29:00 -0700

    Finish getElementsByClassName rough draft needs refactor

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,4 +6,39 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
   // your code here
+  className = className.split(' ');
+  var results = [];
+
+  var containsClass = function(nodeList){
+  	count = 0;
+  	for(var key in nodeList){
+  		if(className.indexOf(nodeList[key]) > -1 ){
+  			count++;
+  	}
+  }
+
+  	if(count == className.length){
+  		return true;
+  	}else {
+  		return false;
+  	}
+  }
+
+ 	var checkNode = function(node){
+ 		if(node.classList){
+ 			if(containsClass(node.classList)){
+ 				results.push(node);
+ 			}
+ 		}
+
+ 		if(node.hasChildNodes()){
+ 			for(var i = 0; i < node.childNodes.length ; i++){
+ 				checkNode(node.childNodes[i]);
+ 			}
+ 		}
+ 	}
+
+ 	checkNode(document.body);
+ 	console.log(results);
+ 	return results;
 };
