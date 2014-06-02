2dd192bb61a749709da74b90b54b2e50c66af41a
Hack Reactor Students <mrwebbin@gmail.com>
2014-06-02 15:02:14 -0700

    Built basic getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,4 +6,21 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
   // your code here
+  var result = [];
+
+  //determine if current node has class name, if so store in result
+  var search = function(node){
+
+    if(node.className && node.className.split(' ').indexOf(className) > -1){
+      result.push(node);
+    }
+    if (node.childNodes){
+      for (var i = 0; i < node.childNodes.length; i++){
+        search(node.childNodes[i]);
+      }
+    }
+
+  };
+  search(document.body);
+  return result;
 };
