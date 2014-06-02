a0ac8b1f99d490042b93019a72d3b16ed4b2c0ac
Hack Reactor Students <mrwebbin@gmail.com>
2014-06-02 16:38:45 -0700

    built strinigfyJSON through arrays

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,4 +4,37 @@
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
   // your code goes here
+  //   console.log(json);
+  var type = typeof obj;
+  var result = [];
+  if(type === 'string'){
+    return '"' + obj + '"';
+  }else if(type === 'number'){
+    return  obj + '';
+  }else if(type === 'boolean'){
+    return obj ? true + '' : false + '';
+  // }else if(type === 'undefined'){
+  //   return obj + '';
+  }else if (type === 'object'){
+    if(obj === null){
+      return null + '';
+    }else if (Array.isArray(obj)){
+      if (obj.length === 0){
+        return '[]';
+      }
+      for(var i = 0; i < obj.length; i++){
+        result.push(stringifyJSON(obj[i]));
+      }
+      return '[' + result.join(',') + ']';
+    }else{
+      if (Object.keys(obj).length === 0){
+        return '{}';
+      }else{
+        for (var key in obj){
+          result.push('"' + key + '":' + stringifyJSON(obj[key]));
+        }
+        return '{' + result.join(',') + '}';
+      }
+    }
+  }
 };
